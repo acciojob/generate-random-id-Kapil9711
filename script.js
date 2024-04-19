968928e1a203c3dfe74c6b6d756d758e3cf1440e
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-04-19 22:24:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+
+	const letter = ['a','B','c','d','e','f','g','h','i','j','k','l','m','N','O',1,2,3,4,5,6,7,8,9];
+	let newStr = ''
+	for(let i = 1; i <=Number(l) ; i++){
+		newStr += letter[Math.floor(Math.random() * 20)];			
+}
+	return newStr;
 }
 
 // Do not change the code below.
